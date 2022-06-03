fbfff8db917551e09b051e3ceb1d867543c0f112
Jessica Raj <jessicaraj75@gmail.com>
2022-06-03 13:48:27 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate(){
     var email= document.getElementById("1").value;
     var pswrd= document.getElementById("2").value;
     if(email=="j@gmail.com" && pswrd == "U@123"){
-        window.location = "https://jessica75raj.github.io/" ;
+        window.location = "https://in.bookmyshow.com" ;
         alert("Login successful");
         return false;
     }
